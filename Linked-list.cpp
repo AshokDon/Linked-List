cd7dc78c98cb6e525f485e00ea0f00751447128c
Ashok <68994450+AshokDon@users.noreply.github.com>
2021-08-04 21:44:00 +0530

    Add files via upload

---
+++ Linked-list.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    
+};
+void Displaydata(Node *n)
+{
+    while(n!=NULL)
+    {
+        cout<<n->data<<" ";
+        n=n->next;
+    }
+}
+
+int main() {
+    //creat the pointer 
+	Node *head = NULL;
+	Node *first=NULL;
+	Node *second=NULL;
+	// allocate the node 
+	head=new Node;
+	first=new Node;
+	second=new Node;
+	
+	head->data=1;
+	head->next=first;
+	
+	first->data=2;
+	first->next=second;
+	
+	second->data=3;
+	second->next=NULL;
+	
+	Displaydata(head);
+	
+	
+	
+	
+	return 0;
+}
