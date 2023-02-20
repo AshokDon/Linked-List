9a1ce4a2687ea64312dc7167e2b0293874481b2b
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-02-20 16:21:15 +0530

    Update Linked-list.cpp

--- Linked-list.cpp
+++ Linked-list.cpp
@@ -1,44 +1,51 @@
-#include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-struct Node{
-    int data;
-    Node* next;
-    
-};
-void Displaydata(Node *n)
-{
-    while(n!=NULL)
-    {
-        cout<<n->data<<" ";
-        n=n->next;
-    }
+typedef long long ll;
+void TakeInput(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 }
 
-int main() {
-    //creat the pointer 
-	Node *head = NULL;
-	Node *first=NULL;
-	Node *second=NULL;
-	// allocate the node 
-	head=new Node;
-	first=new Node;
-	second=new Node;
-	
-	head->data=1;
-	head->next=first;
-	
-	first->data=2;
-	first->next=second;
-	
-	second->data=3;
-	second->next=NULL;
-	
-	Displaydata(head);
-	
-	
-	
-	
-	return 0;
+class LinkedList{
+    struct  Node{
+        int data;
+        Node *next;
+        Node(int val){
+            data = val;
+            next = NULL;
+        }
+    };
+    
+    Node *head;
+public:
+    LinkedList(){
+        head = NULL;
+    }
+    void InsertNode(int data){
+        Node *newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next = newNode;
+    }
+    void de
+
+};
+int main()
+{
+    TakeInput();
+    Node *head = new Node(10);
+    Node *first = new Node(20);
+    Node *second = new Node(30);
+    head->next = first;
+    first->next = second;
+    PrintList(head);
+
 }
