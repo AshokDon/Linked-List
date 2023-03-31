d5126e9ee6f84b7365656b050c74a6802a24715b
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-03-31 12:03:06 +0530

    Update Linked-list.cpp

--- Linked-list.cpp
+++ Linked-list.cpp
@@ -1,74 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-void TakeInput(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+struct Node *head = NULL;
+struct Node *tail = NULL;
+struct Node *Creat_A_NewNode(int val){
+    struct Node *nn = (struct Node*)malloc(sizeof(struct Node));
+    nn->data = val;
+    nn->next = NULL;
+    return nn;
 }
-
-class LinkedList{
-public:
-    struct Node
-    {
-        int data;
-        Node *next;
-        Node(int val){
-            data = val;
-            next = NULL;
-        }
-    };
-    Node *head;
-    Node *tail;
-    LinkedList(){
-        head = NULL;
-        tail = NULL;
+void PrintALinkedList(struct Node *head){
+    struct Node *temp = head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp = temp->next;
     }
-    void Insert(int x){
-        //creat a new node 30,NULL
-        Node *newNode = new Node(x);
-        if(head==NULL){
-            head = newNode;
-            tail = newNode
-            return;
-        }
-        tail->next = newNode;
+}
+void Creat_A_LInked_List(int val){
+    struct Node* nn = Creat_A_NewNode(val);
+    if(head == NULL){
+       // printf("hi");
+        head = nn;
+        tail = nn;
+        
+    }
+    else{
+        tail->next = nn;
         tail = tail->next;
     }
-    void Printdata(){
-        Node *temp = head;
-        while(temp){
-            cout<<temp->data<<" ";
-            temp = temp->next;
-        }
-    }
-    void InsertAtBegin(int x){
-        //fill this fuction
-
-    }
-    void InsertAtEnd(int x){
-        //fill this fuction
-    }
-    void InsertAtPosition(int x,int pos){
-        //fill this function
-    }
-
-};
-int main(){
-    TakeInput();
-    LinkedList list;
-    list.Insert(10);
-    list.Insert(20);
-    list.Insert(30);
-    //insert at begin
-    //insert at end 
-    //insert in middle
-
-    list.Printdata();
-
+    
+    
 }
-
-
-
-
+int main(){
+    for(int i = 0 ; i < 5 ; i++){
+        int val;
+        scanf("%d",&val);
+        Creat_A_LInked_List(val);
+    }
+    PrintALinkedList(head);
+    
+}
