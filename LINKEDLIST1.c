92289c5a9d8ed93028b634aec5a20e2e9e4be4e7
Ashok <68994450+AshokDon@users.noreply.github.com>
2024-01-25 10:33:15 +0530

    Update LINKEDLIST1.c

--- LINKEDLIST1.c
+++ LINKEDLIST1.c
@@ -1,24 +1,31 @@
-//Linked list
-#include <stdio.h>
-#include <stdlib.h>
-
+//linkedlist creation
+#include<stdio.h>
+#include<stdlib.h>
 struct Node{
     int data;
     struct Node *next;
 };
+
 int main(){
     struct Node *head = NULL;
     struct Node *second = NULL;
     struct Node *third = NULL;
 
-    //allocate 3 nodes in the heap
+    //allocate memory for nodes in heap
     head = (struct Node*)malloc(sizeof(struct Node));
     second = (struct Node*)malloc(sizeof(struct Node));
     third = (struct Node*)malloc(sizeof(struct Node));
+
+    //link first and second nodes
     head->data = 1;
-    second->data = 2;
-    third->data = 3;
     head->next = second;
+
+    //link second and third nodes
+    second->data = 2;
     second->next = third;
+
+    //terminate the list at the third node
+    third->data = 3;
     third->next = NULL;
+    
 }
